0b935ee3d31d2e4220e3b527ed5409d1ccd5b9cf
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-11-18 21:09:39 +1100

    Add login step to tf apply

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name  = "iac"
     storage_account_name = "fomojisterraform"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "${var.base_name}-terraform.tfstate"
     use_oidc             = true
   }
 }
