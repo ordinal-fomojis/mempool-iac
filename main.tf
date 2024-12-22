7370808a4f340de8bd3bea1fd4e1780f104b06b4
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-22 19:12:04 +1100

    Add random dns prefix

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.14.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
   }
 
   backend "azurerm" {
@@ -24,11 +28,20 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-cluster"
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
   location            = var.location
   resource_group_name = var.rg_name
-
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  
   default_node_pool {
     name       = "default"
     node_count = 2
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,7 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
+}
