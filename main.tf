20f5a857e8592d2d147d9baa776e925d262f2091
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-22 21:54:49 +1100

    Update VM Size (#9)

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2_v5"
+    vm_size    = "Standard_D2ps_v5"
   }
 
   identity {
