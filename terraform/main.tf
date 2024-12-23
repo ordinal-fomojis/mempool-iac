01c9e2b300716b231720d6b21b511a0b7d6e84b1
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-24 10:12:50 +1100

    Change acr name (#35)

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ resource "random_string" "acr_name" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${random_string.acr_name.result}-registry"
+  name                = "${random_string.acr_name.result}registry"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
