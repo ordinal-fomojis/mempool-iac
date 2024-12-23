c3b1b73353b48caea5c4c1772ff1134ebd6879bb
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-24 08:29:20 +1100

    Add prod and nonprod environments for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "iac"
     storage_account_name = "fomojisterraform"
-    container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
