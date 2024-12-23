d5b44ca0028cb2cbbfcaa079c6c1b21d736d0a8d
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-23 16:52:36 +1100

    Add persistent volume claim

---
+++ local/kubectl-init.sh
@@ -0,0 +1,5 @@
+export ARM_SUBSCRIPTION_ID=cc174810-b2c5-4ca2-8397-0c701e8a2b96
+cd terraform
+rg_name=$(terraform output -raw rg_name)
+cluster_name=$(terraform output -raw k8s_cluster_name)
+az aks get-credentials --resource-group $rg_name --name $cluster_name
