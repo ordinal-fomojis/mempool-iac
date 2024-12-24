fc6afd594d0cadd43db48717f168d445589256b5
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-24 16:35:29 +1100

    Add role assignment

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,10 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
+
+resource "azurerm_role_assignment" "acr_role" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
