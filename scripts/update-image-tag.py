452dda5fb65f7fa7581f0f32aceebf5d22408b9e
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-26 19:17:47 +1100

    Fix updater script

--- scripts/update-image-tag.py
+++ scripts/update-image-tag.py
@@ -8,17 +8,16 @@ IMAGE = os.environ.get('IMAGE')
 def main():
     if IMAGE is None:
         raise ValueError('IMAGE environment variable is required')
-    manifests = glob.glob('k8s/bitcoin/*.yaml')
-    for manifest in manifests:
-        # print(manifest)
-        with open(manifest, 'r') as f:
+    manifests_paths = glob.glob('k8s/bitcoin/*.yaml')
+    for manifest_path in manifests_paths:
+        with open(manifest_path, 'r') as f:
             manifest = list(yaml.safe_load_all(f))
         for obj in manifest:
             if obj['kind'] != 'StatefulSet':
                 continue
             obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
 
-        with open(manifest, 'w') as f:
+        with open(manifest_path, 'w') as f:
             yaml.dump_all(manifest, f)
 
 
