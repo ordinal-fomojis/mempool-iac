4d65d66981d30c3dd4d685cd87f6a6784aca7d72
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-26 19:15:24 +1100

    Add mainnet and testnet manifests

--- scripts/update-image-tag.py
+++ scripts/update-image-tag.py
@@ -1,19 +1,25 @@
 import os
+import glob
 import yaml
 
-IMAGE = os.environ.get('IMAGE', 'not-set')
+IMAGE = os.environ.get('IMAGE')
 
 
 def main():
-    with open('k8s/bitcoin/manifest.yaml', 'r') as f:
-        manifest = list(yaml.safe_load_all(f))
-    for obj in manifest:
-        if obj['kind'] != 'StatefulSet':
-            continue
-        obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
+    if IMAGE is None:
+        raise ValueError('IMAGE environment variable is required')
+    manifests = glob.glob('k8s/bitcoin/*.yaml')
+    for manifest in manifests:
+        # print(manifest)
+        with open(manifest, 'r') as f:
+            manifest = list(yaml.safe_load_all(f))
+        for obj in manifest:
+            if obj['kind'] != 'StatefulSet':
+                continue
+            obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
 
-    with open('k8s/bitcoin/manifest.yaml', 'w') as f:
-        yaml.dump_all(manifest, f)
+        with open(manifest, 'w') as f:
+            yaml.dump_all(manifest, f)
 
 
 if __name__ == '__main__':
