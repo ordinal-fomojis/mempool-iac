d0697769c9ba178c276edf8eb4c65acc0cc774c4
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-26 19:24:53 +1100

    Add the ability to deploy to testnet and mainnet (#66)
    * Add mainnet and testnet manifests
    
    * Fix updater script
    
    * Add options to deploy to mainnet or testnet

--- scripts/update-image-tag.py
+++ scripts/update-image-tag.py
@@ -1,19 +1,24 @@
 import os
+import glob
 import yaml
 
-IMAGE = os.environ.get('IMAGE', 'not-set')
+IMAGE = os.environ.get('IMAGE')
 
 
 def main():
-    with open('k8s/bitcoin/manifest.yaml', 'r') as f:
-        manifest = list(yaml.safe_load_all(f))
-    for obj in manifest:
-        if obj['kind'] != 'StatefulSet':
-            continue
-        obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
+    if IMAGE is None:
+        raise ValueError('IMAGE environment variable is required')
+    manifests_paths = glob.glob('k8s/bitcoin/*.yaml')
+    for manifest_path in manifests_paths:
+        with open(manifest_path, 'r') as f:
+            manifest = list(yaml.safe_load_all(f))
+        for obj in manifest:
+            if obj['kind'] != 'StatefulSet':
+                continue
+            obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
 
-    with open('k8s/bitcoin/manifest.yaml', 'w') as f:
-        yaml.dump_all(manifest, f)
+        with open(manifest_path, 'w') as f:
+            yaml.dump_all(manifest, f)
 
 
 if __name__ == '__main__':
