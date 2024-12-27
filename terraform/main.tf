ef9dba3efb4dea03ae0c97ba36a6455281ac07ff
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-27 12:25:08 +1100

    Add keyvault (#77)
    * Add keyvault
    
    * Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,8 @@ provider "azurerm" {
   }
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
@@ -80,4 +82,35 @@ resource "azurerm_role_assignment" "acr_role" {
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
+}
+
+############ Azure Key Vault ############
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "keyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "random_password" "rpc-password" {
+  length = 32
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "bitcoin-rpc-password"
+  value        = random_password.rpc-password.result
+  key_vault_id = azurerm_key_vault.kv.id
 }
