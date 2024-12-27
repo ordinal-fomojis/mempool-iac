5bf01c941c321796ef9c328215c6529c3166084c
Vannix <134819232+Vanniix@users.noreply.github.com>
2024-12-27 12:10:49 +1100

    Only update yamls that are edited (#76)

--- scripts/update-image-tag.py
+++ scripts/update-image-tag.py
@@ -12,11 +12,16 @@ def main():
     for manifest_path in manifests_paths:
         with open(manifest_path, 'r') as f:
             manifest = list(yaml.safe_load_all(f))
+        edited = False
         for obj in manifest:
             if obj['kind'] != 'StatefulSet':
                 continue
+            edited = True
             obj['spec']['template']['spec']['containers'][0]['image'] = IMAGE
 
+        if not edited:
+            continue
+        
         with open(manifest_path, 'w') as f:
             yaml.dump_all(manifest, f)
 
