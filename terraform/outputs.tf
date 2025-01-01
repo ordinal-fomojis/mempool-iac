428306cc5437d4f7c32211a1c5c99aef42bfc984
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-01-01 18:17:06 +1100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,7 +20,12 @@ output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
 
-output "btc_rpc_password" {
-  value     = random_password.btc-rpc-password.result
+output "mainnet_rpc_password" {
+  value     = random_password.mainnet-rpc-password.result
+  sensitive = true
+}
+
+output "testnet_rpc_password" {
+  value     = random_password.testnet-rpc-password.result
   sensitive = true
 }
